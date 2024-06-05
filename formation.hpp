72857e4ead3ef3d72c0e7e0476cc01b37ce2d749
Agatha <fred0412@live.com>
2024-06-05 20:45:04 +0800

    构想了几个关键类函数

--- formation.hpp
+++ formation.hpp
@@ -1,3 +1,19 @@
+/**
+  ******************************************************************************
+  * @author  Zhang weizhi
+  * @date    5-Jun-2024
+  * @brief   Drone-swarm-avoidance-cooperative-target
+  ******************************************************************************
+
+ * Function simpleAtof 不依赖库函数转换浮点为double
+ * Function read_calibration_table 读取存在table.c中的字符串数组 table.c用于存校准表
+
+ */
+
+int start_frame = 25;			// 开始帧
+double constraint_speed = 6;		// 速度约束
+// 避碰半径
+
 typedef struct vec3d	vec3d;
 struct vec3d {
 	double	x;
@@ -10,11 +26,19 @@ struct set3d {
 	double	x;
 	double	y;
 	double	z;
-    double  timestamp;
+    double  frame;
 };
 
 class drone
 {
-    public:
-    std::vector<set3d> SET;
+	public:
+		int drone_ID;
+		std::vector<set3d> SET;
+		drone(int);
+		// void export(int frame);			//TODO 给帧数输出对应位置
+		// void get_radio(int frame, /*未来几帧轨迹*/);		// TODO 接收广播轨迹
+		// void whether_collision();		// 判断轨迹是否重合
+		// void avoid(/*应该return 一个指导向量,不一定要打卡位置*/);					// 规避机动策略
+
 };
+
--- main.c
+++ main.c
@@ -16,6 +16,7 @@ std::vector<std::string> split(const std::string& s, char delimiter) {
     return tokens;
 }
 
+std::string filename_base = "E:\\Project_C\\Yielded_240605\\";
 std::string map(int num)
 {
     if (num == 0) return("datax.csv");
@@ -24,8 +25,33 @@ std::string map(int num)
 } 
 
 
+void set_per_drone(size_t control_bit, drone per)
+{
+    switch (control_bit)
+    {
+        case 0:
+            // Code for control_bit 0
+            break;
+        case 1:
+            // Code for control_bit 1
+            break;
+        case 2:
+            // Code for control_bit 2
+            break;
+    }
+}
+
+drone::drone(int id) {
+		drone_ID = id;
+}
+
 int main() {
-    std::string filename_base = "E:\\Project_C\\Yielded_240605\\";
+    std::vector<drone> per_drone;       // 创建机群容器
+    for (int i = 0; i < 1934; ++i) {     // 创建1934架次
+        drone obj(i);
+        per_drone.push_back(obj);
+    }
+
 
     for (size_t i = 0; i < 3; i++)
     {
@@ -40,15 +66,26 @@ int main() {
         std::vector<std::vector<std::string>> data;     // 二维vector
         std::string line;
 
+        // while (std::getline(file, line)) {
+        //     std::vector<std::string> row = split(line, ',');
+        //     data.push_back(row);
+        // }
+
         while (std::getline(file, line)) {
             std::vector<std::string> row = split(line, ',');
-            data.push_back(row);
+            for (size_t j = 0; j < row.size(); i++)
+            {   
+                auto per = per_drone[i];
+                // set_per_drone(j, per);
+                printf("num:%d", i);
+            }
+            
         }
 
         file.close();
     }
 
-
+    // 设置三个通道 舞步, 经过算法的变轨(不代表实际位置,视为满足位置约束),计算的补位飞机轨迹
 
 
 
